ffa68f4760fdce1efa177caf8539e1cb39a158c8
byguesswork <byguesswork@gmail.com>
2022-08-09 22:35:18 +0200

    innerWidth fixes for mobile devices for index and privacy

--- app/igrajmose/privacy/privacy.js
+++ app/igrajmose/privacy/privacy.js
@@ -1,6 +1,13 @@
 'use strict';
 
 const test = document.getElementById('test');
+const mainDiv = document.querySelector('.main');
+
+if (window.innerWidth > screen.width) {
+    const width = screen.width - 40;
+    mainDiv.style.width = `${width}px`;
+}
 
 test.textContent = `window.innerWidth: ${window.innerWidth}, screen.width: ${screen.width}\n
 window.inerHeight: ${window.innerHeight}, screen.height: ${screen.height}`;
+
--- index.js
+++ index.js
@@ -4,13 +4,17 @@
 
 const leftSideIndexIfMobiles = document.querySelectorAll('.if_mobile');
 const rightSideIndex = document.querySelector('.right-index');
+const mainLeftSide = document.querySelector('.main');
 
-if (window.innerWidth < 500) {
+if (window.innerWidth < 500 || screen.width < 500) {
     leftSideIndexIfMobiles.forEach((p) => {
         p.classList.remove('hidden');
     });
 
     rightSideIndex.classList.add('hidden');
+
+    const width = window.innerWidth > screen.width ? screen.width - 40 : window.innerWidth - 40;
+    mainLeftSide.style.width = `${width}px`
 
 }
 
