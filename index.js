15e3df644b2eec8065ff6a094963096d538d4654
byguesswork <byguesswork@gmail.com>
2022-08-15 12:41:08 +0200

    new version of site (+test pages deleted)

--- index.js
+++ index.js
@@ -2,39 +2,150 @@
 
 // ----------------     SELEKTORJI
 
-const leftSideIndexIfMobiles = document.querySelectorAll('.if_mobile');
-const rightSideIndex = document.querySelector('.right-index');
-const mainLeftSide = document.querySelector('.main');
-const mainleftSideUpperPart = document.querySelector('.main_upper_part');
-const mainleftSideBottomPart = document.querySelector('.bottom');
-const aboutRightSideBlabber = document.querySelector('.main-blabber');
-const aboutRightSide = document.querySelector('.right-about');
+const leftContainer = document.getElementById('left_container');
+const leftUpperContent = document.getElementById('left_upper_content');
+const leftLowerContent = document.getElementById('left_lower_content');
+const rightContainer = document.getElementById('right_container');
+const rightUpperContent = document.getElementById('right_upper_content');
+const rightLowerContent = document.getElementById('right_lower_content');
+
+const hideIfMobiles = document.querySelectorAll('.hide_if_mobile');
+const showIf1col = document.querySelectorAll('.show_if_1_col');
+const hideIf1col = document.querySelectorAll('.hide_if_1_col');
+const moveLeftIfMobiles = document.querySelectorAll('.move_left_if_mobile');
+
+let lesserWidth;
+let isMobile;
 
 
-if (window.innerWidth < 500 || screen.width < 500) {
-    leftSideIndexIfMobiles.forEach((p) => {
-        p.classList.remove('hidden');
-    });
+//  če je manj kot 441  : en stolpec
+// če je med 441 in 730 : dva stolpca (360 + 370, vmes je border 1px), horizontalni skrol; 2. stolpec je končno ravno nekoliko večji od prvega
+// če je med 731 in 800 : dva stolpca (360 + 370–440, vmes je border 1px), brez H skrola
+// če je med 801 in 960 : dva stolpca (400 + 400–560, vmes je border 1px), brez H skrola
+// če je nad 960:       : dva stolpca (400 + 560, vmes je border 1px)
 
-    rightSideIndex?.classList.add('hidden'); // skrije desni del pri indexu
-    aboutRightSide?.classList.add('hidden'); // skrije desni del pri disclaimerju
-
-    const width = window.innerWidth > screen.width ? screen.width - 40 : window.innerWidth - 40; // -40 ker je padding 20 levo in desno
-    mainLeftSide.style.width = `${width}px`
-
-} else if (mainleftSideUpperPart != null) { // za index in disclaimer, ki imata tako strukturo
-    if ((mainleftSideUpperPart.getBoundingClientRect().height + mainleftSideBottomPart.getBoundingClientRect().height + 30) < window.innerHeight) {
-        // to je zato, ker če ne na računalnku zabije dno gor in del teksta ni videm, če tega ni
-        // če pa je to prisotno pri mobilcu, je dno zaklenjeno in ne skrola in spet ni vidno vse besedilo, zato se doda samo na računalniku pri zadostni višini
-        mainLeftSide.style.bottom = "0";
-        mainleftSideBottomPart.style.position = "absolute";
-        aboutRightSide.style.position = "relative";
-    }
-} else if (aboutRightSideBlabber.getBoundingClientRect().height > window.innerHeight) {  // v tem primeru gre za about, ki ima svojo strukturo
-    aboutRightSide.style.position = "relative";
-    document.getElementsByTagName('body')[0].style.background = "#808080";
+function init() {
+  if (navigator.userAgent.match(/(android|iphone|ipad)/i) != null || navigator.userAgentData.mobile == true) isMobile = true;  // todo to bi veljalo izboljšat s čeiranjem še širine
 }
 
+function doLayout() {
+
+  // najprej čekiramo & prilagodimo širino
+  lesserWidth = document.documentElement.clientWidth < screen.width ? document.documentElement.clientWidth : screen.width;
+
+  if (lesserWidth < 441) goForOneColumn();
+  else {
+    goForTwoColumns();
+
+    //  todo reformat spodnje
+    if (lesserWidth >= 441 && lesserWidth <= 730) {  // tle pustimo horizontalni skrol
+      leftContainer.style.width = `${360 - 40}px`;  // 360, ker je taka ciljna širina 1. stolpca, in 40, ker je tak padding 1. stolpca
+      rightContainer.style.left = `362px`           // ker je ciljna širina 1. stolpca 360 + 1 za border;
+      rightContainer.style.width = `${730 - (360 + 40 + 1 + 1)}px`;  // širina 2. stolpca je arbitrarna meja 730 - (celotna širina 1. stolpca + padding 2. stolpca + border + rezerva, ker če ne se pojavi Hskrolbar);
+    }
+    if (lesserWidth >= 731 && lesserWidth <= 800) {  // brez horiz. skrola
+      leftContainer.style.width = `${360 - 40}px`;  // 360, ker je taka ciljna širina 1. stolpca, in 40, ker je tak padding 1. stolpca
+      rightContainer.style.left = `362px`           // ker je ciljna širina 1. stolpca 360 + 1 za border;
+      rightContainer.style.width = `${lesserWidth - (360 + 40 + 1 + 1)}px`;  // širina 2. stolpca je razpoložljiva širina - (celotna širina 1. stolpca + padding 2. stolpca + border + rezerva, ker če ne se pojavi Hskrolbar);
+    }
+    if (lesserWidth >= 801 && lesserWidth <= 960) {  // brez horiz. skrola
+      leftContainer.style.width = `${400 - 40}px`;  // 400, ker je taka ciljna širina 1. stolpca, in 40, ker je tak padding 1. stolpca;
+      rightContainer.style.left = `402px`           // ker je ciljna širina 1. stolpca 400 + 1 za border;
+      rightContainer.style.width = `${lesserWidth - (400 + 40 + 1 + 1)}px`;  // širina 2. stolpca je razpoložljiva širina - (celotna širina 1. stolpca + padding 2. stolpca + border + rezerva, ker če ne se pojavi Hskrolbar);
+    }
+    if (lesserWidth >= 961) {  // brez horiz. skrola
+      leftContainer.style.width = `${400 - 40}px`;  // 400, ker je taka ciljna širina 1. stolpca, in 40, ker je tak padding
+      rightContainer.style.left = `402px`           // ker je ciljna širina 1. stolpca 400 + 1 za border;
+      rightContainer.style.width = `${560 - (40 + 1 + 1)}px`;  // širina 2. stolpca je ciljna širina (560) - (padding 2. stolpca + border + rezerva, ker če ne se pojavi Hskrolbar);
+    }
+  }
+
+  // if (isMobile) {
+  //   let ratio = ((leftContainer.getBoundingClientRect().width + rightContainer?.getBoundingClientRect().width) / screen.width);
+  //   // document.body.style.transform = `scale(${1 / ratio})`;   115 - to ne dela OK, morda treba kaj dodat
+  // }
+
+  // potem postavimo elemente, ki vplivajo na višino, ter čekiramo & prilagodimo višino
+  if (isMobile) {
+    hideIfMobiles?.forEach((el) => el.classList.add('hidden'))
+  } else hideIfMobiles?.forEach((el) => el.classList.remove('hidden'));
+
+  checkAbsolutes();
+
+}
+
+function goForOneColumn() {
+
+  //  prilagodimo širino vsebine levega, edinega stolpca
+  leftContainer.style.width = lesserWidth > 300 ? `${lesserWidth - 40}px` : '260px';  // -40, ker je padding 40; minimalno mora bit vsebina široka 260px;
+  leftContainer.style.borderRight = 'unset';
+
+  //  kjerje to potrebno, prikažemo, skrijemo (v prihodnje morda tudi premaknemo) vsebino
+
+  // najprej prikažemo show_if_mobile, nato skrijemo hide_if_mobile, s čimer se skrije tudi desni stolpec
+  showIf1col?.forEach((i) => i.classList.remove('hidden'));
+  hideIf1col?.forEach((i) => i.classList.add('hidden'));
+
+}
+
+function goForTwoColumns() {
+  leftContainer.style.borderRight = 'solid 1px #f0fff0';
+  showIf1col?.forEach((i) => i.classList.add('hidden'));
+  hideIf1col?.forEach((i) => i.classList.remove('hidden'));
+}
+
+function checkAbsolutes() {
+
+  // poiščemo, koliko največ vidi uporabnik na zaslonu
+  let lesserHeight = document.documentElement.clientHeight < screen.height ? document.documentElement.clientHeight : screen.height;
+
+  // preverimo, ali vsebina levega ALI desnega stolpca sega globlje od spodnjega roba 
+  if (leftUpperContent?.getBoundingClientRect().height + leftLowerContent?.getBoundingClientRect().height + 20 > lesserHeight ||   // ta plus 20 je zato, da se upošteva tudi padding nad in pod levim kontejnerjem, ki sicer ni vštet v višino posmičnih elementov;
+    rightUpperContent?.getBoundingClientRect().height + rightLowerContent?.getBoundingClientRect().height + 20 > lesserHeight) {
+
+    // uredimo za levi stolpec
+    leftContainer.style.bottom = 'unset';
+    leftLowerContent.style.position = 'static';
+
+    //  še za desni stolpec
+    rightContainer.style.bottom = 'unset';
+
+    // podaljšat levega, če desni predolg, in obratno
+    if (rightContainer.getBoundingClientRect().height > leftContainer.getBoundingClientRect().height) leftContainer.style.height = `${rightContainer.getBoundingClientRect().height - 40}px`; // -40 ker padding
+    if (leftContainer.getBoundingClientRect().height > rightContainer.getBoundingClientRect().height) rightContainer.style.height = `${leftContainer.getBoundingClientRect().height - 40}px`; // -40 ker padding
+
+    // za about.html, spravit vsebino pod fiksni del
+    if (leftUpperContent.classList.contains('fixed')) {
+      leftUpperContent.style.width = `${leftContainer.getBoundingClientRect().width - 20 - 1}px`; // -20 ker je treba odštet levi padding, -1, ker je teba odštet vidni border;
+      leftLowerContent.style.marginTop = `${leftUpperContent.getBoundingClientRect().bottom}px`;
+
+      //test
+      leftContainer.style.paddingTop = '0';       // zakaj tukaj vzet 20px (0) in spodaj dodat 20px (40px) ?;
+      leftContainer.style.paddingBottom = '40px'; // ker skupni vertikalni padding levega kontejnerja mora še vedno biti 40, sicer se levi in desni kontejner razlikujeta po višini;
+      leftUpperContent.style.paddingTop = '20px'; // tako se naredi, da ni nad fiksnim delom prozornega dela, v katerem bi se videlo besedilo od about, ki ga skrolaš navzgor
+
+      // konec test
+
+    }
+
+  } else {  // če je vse na enem ekranu, prilepimo dno vsebine na dno ekrana
+    // levi stolpec
+    leftContainer.style.bottom = '0';
+    leftLowerContent.style.position = 'absolute';
+
+    //  desni stolpec
+    rightContainer.style.bottom = '0';
+  }
+
+}
+
+
+//  - - - - - - - - -  IZVAJANJE  - - - - - -
+
+init();
+doLayout();
+
+if (isMobile) screen.orientation.addEventListener("change", () => { doLayout(); });
 
 //  coded with love and by guesswork by Ivo Makuc, 2022
 //  byguesswork@gmail.com
