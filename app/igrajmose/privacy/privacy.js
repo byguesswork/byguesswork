e23ae79f8ad30817f3fbee07a549515e3b5f4788
byguesswork <byguesswork@gmail.com>
2022-10-16 12:51:50 +0200

    tweak videza policy

--- app/igrajmose/privacy/privacy.js
+++ app/igrajmose/privacy/privacy.js
@@ -4,7 +4,7 @@ const test = document.getElementById('test');
 const mainDiv = document.querySelector('.main');
 
 if (window.innerWidth > screen.width) {
-    const width = screen.width - 46;  // toliko sta horiz paddinga
+    const width = screen.width - 50;  // horiz. padinga + margin + 10px, da je na desni malo zelene
     mainDiv.style.width = `${width}px`;
 }
 
