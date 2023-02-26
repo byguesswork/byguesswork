efb1e8141011dc6c2193971b8a0b73fdcf2d1e72
byguesswork <byguesswork@gmail.com>
2023-02-26 21:45:02 +0100

    plug-ins, final

--- random/random.js
+++ random/random.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const mainDocElement = document.querySelector('.main');
+const td = document.querySelectorAll('td');
 
 let isMobile;
 if (navigator.userAgent.match(/(android|iphone|ipad)/i) != null || navigator.userAgentData.mobile == true) {
@@ -12,4 +13,6 @@ if (navigator.userAgent.match(/(android|iphone|ipad)/i) != null || navigator.use
 if (isMobile) {
     mainDocElement.style.marginRight = '30px';
     mainDocElement.style.marginLeft = '20px';
+    td.forEach((el) => el.style.fontSize = '12px');
+
 }
