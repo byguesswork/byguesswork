8b5bb2de92d7d33653e976703aa02c72e2bf44cb
byguesswork <byguesswork@gmail.com>
2023-06-09 23:55:30 +0200

    resize za random

--- random/random.js
+++ random/random.js
@@ -14,5 +14,13 @@ if (isMobile) {
     mainDocElement.style.marginRight = '30px';
     mainDocElement.style.marginLeft = '20px';
     td.forEach((el) => el.style.fontSize = '12px');
+} else atResize();
 
+function atResize() {
+    if (window.innerWidth > 1280) {
+        let newRightMargin = window.innerWidth / 2.5;
+        mainDocElement.style.marginRight = `${newRightMargin}px`;
+    } else mainDocElement.style.marginRight = `100px`;
 }
+
+window.addEventListener("resize", atResize);
