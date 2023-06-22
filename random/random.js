6465f9fd6a67265bfe1b009375e58d24fe24cf10
byguesswork <byguesswork@gmail.com>
2023-06-22 02:32:42 +0200

    lemans

--- random/random.js
+++ random/random.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const mainDocElement = document.querySelector('.main');
-const td = document.querySelectorAll('td');
 
 let isMobile;
 if (navigator.userAgent.match(/(android|iphone|ipad)/i) != null || navigator.userAgentData.mobile == true) {
@@ -13,14 +12,20 @@ if (navigator.userAgent.match(/(android|iphone|ipad)/i) != null || navigator.use
 if (isMobile) {
     mainDocElement.style.marginRight = '30px';
     mainDocElement.style.marginLeft = '20px';
+    const td = document.querySelectorAll('td');
     td.forEach((el) => el.style.fontSize = '12px');
 } else atResize();
 
 function atResize() {
-    if (window.innerWidth > 1280) {
-        let newRightMargin = window.innerWidth / 2.5;
-        mainDocElement.style.marginRight = `${newRightMargin}px`;
-    } else mainDocElement.style.marginRight = `100px`;
+    if (window.innerWidth > 930) {
+        if (window.innerWidth > 2400) {
+            let newRightMargin = window.innerWidth * 0.61;
+            mainDocElement.style.marginRight = `${newRightMargin}px`;
+        } else {
+            let newRightMargin = window.innerWidth - 930;
+            mainDocElement.style.marginRight = `${newRightMargin}px`;
+        }
+    } else mainDocElement.style.marginRight = `20px`;
 }
 
 window.addEventListener("resize", atResize);
