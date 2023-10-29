2a8d51c824363400cf7ef57a200e15df764c73c9
byguesswork <byguesswork@gmail.com>
2023-10-29 20:23:27 +0100

    poravit delovanje slike na mobilnih napravah, crooked

--- random/random_crooked_key.js
+++ random/random_crooked_key.js
@@ -1,8 +1,13 @@
 'use strict';
 
-// dat manjšo sliko pri ozkem ekranu
-if (window.innerWidth < 640 || screen.innerWidth < 640) {
-    const pic = document.getElementById('pic');
-    pic.setAttribute("src", "/images/random_crooked_300w.jpg");
+// dat manjšo sliko pri ozkem ekranu oz. normalno pri večjem
+const pic = document.getElementById('pic');
+const picDiv = document.getElementById('pic-div');
+if (window.innerWidth > 640 && screen.width > 640 && mainDocElement.getBoundingClientRect().width > 640) {
+    // samo spremenimo source, na večjega, višina div-a pa je že naštimana za večjo sliko;
+    pic.setAttribute("src", "/images/random_crooked_600w.jpg");
+} else {
+    // moramo popravit višino (na div-u), ker je izvirno naštimana za večjo sliko; src je privzeto mala slika;
+    picDiv.style.height = "168px";
 }
-isMobile
+
