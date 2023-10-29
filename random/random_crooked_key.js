7555e0b552006943f61bcb9d0a5da9eabe676257
byguesswork <byguesswork@gmail.com>
2023-10-29 17:51:56 +0100

    Update random_crooked_key.js

--- random/random_crooked_key.js
+++ random/random_crooked_key.js
@@ -1,7 +1,8 @@
 'use strict';
 
 // dat manjšo sliko pri ozkem ekranu
-if (window.innerWidth < 640) {
+if (window.innerWidth < 640 || screen.innerWidth < 640) {
     const pic = document.getElementById('pic');
     pic.setAttribute("src", "/images/random_crooked_300w.jpg");
 }
+isMobile
