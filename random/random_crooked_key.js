41ab742bca225c83b103c444ee39a6402884affe
byguesswork <byguesswork@gmail.com>
2023-10-30 12:35:43 +0100

    crooked og

--- random/random_crooked_key.js
+++ random/random_crooked_key.js
@@ -5,7 +5,7 @@ const pic = document.getElementById('pic');
 const picDiv = document.getElementById('pic-div');
 if (window.innerWidth > 640 && screen.width > 640 && mainDocElement.getBoundingClientRect().width > 640) {
     // samo spremenimo source, na večjega, višina div-a pa je že naštimana za večjo sliko;
-    pic.setAttribute("src", "/images/random_crooked_600w.jpg");
+    pic.setAttribute("src", "../images/random_crooked_600w.jpg");
 } else {
     // moramo popravit višino (na div-u), ker je izvirno naštimana za večjo sliko; src je privzeto mala slika;
     picDiv.style.height = "168px";
