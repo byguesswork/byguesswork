1e55eac04d2c78569d710fb8e6cdb15f4a9e59f5
byguesswork <byguesswork@gmail.com>
2025-04-15 09:35:06 +0200

    minor twiks

--- app/app.js
+++ app/app.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const mainDocElement = document.querySelector('.main');
-let imgs;
-let lesserBoundingHorizontal
+// let imgs;
+// let lesserBoundingHorizontal
 
 let isMobile;
 if (navigator.userAgent.match(/(android|iphone|ipad)/i) != null || navigator.userAgentData.mobile == true) {
@@ -16,17 +16,17 @@ if (isMobile) {
     mainDocElement.style.marginRight = '30px';
     mainDocElement.style.marginLeft = '20px';
 
-    // za Fuel;
-    lesserBoundingHorizontal = window.innerWidth > screen.width ? screen.width : window.innerWidth;
+    // predvsem za Fuel, ker Igrajmo se ima majhne slike, povrh imajo velikost določeno na element level;
+    const lesserBoundingHorizontal = window.innerWidth > screen.width ? screen.width : window.innerWidth;
     console.log(lesserBoundingHorizontal)
-    imgs = document.getElementsByClassName('pics');
+    const imgs = document.getElementsByClassName('pics');
     for (let i = 0; i < imgs.length; i++) {
-        if (imgs[i].width > lesserBoundingHorizontal - 50 /* 20 margin mainDoc + 20, kolikor je marginL slike + 10, kolikor je margR slike; */
+        if (imgs[i].width >= lesserBoundingHorizontal - 60 /* 20 margin mainDoc + 20 (marginL slike) + 20 (margR slike); */
             && imgs[i].width < imgs[i].height /* samo za pokončne slike za zdaj */ ) {
-                console.log('ja')
-                imgs[i].style.marginLeft = '0px';
-                imgs[i].style.marginRight = '20px';
-                imgs[i].width = lesserBoundingHorizontal - 40;  // ker zdaj je 20 margin mainDoc + 20 marginR slike; marginR mainDoc (30?) ne vpliva (vsaj ne, če imaš vodoravne slike ali ne)
+                imgs[i].style.marginLeft = '20px';
+                imgs[i].style.marginRight = '40px';
+                imgs[i].style.marginBottom = '30px';
+                imgs[i].width = lesserBoundingHorizontal - 80;  // ker zdaj je 20 margin mainDoc + 20 margin L slike + 40 marginR slike; marginR mainDoc (30?) ne vpliva (vsaj ne, če imaš vodoravne slike ali ne)
 
         }
     }
