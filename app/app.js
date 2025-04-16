b4b18c491b27a1113f75f223ff83e02b4bb9b192
byguesswork <byguesswork@gmail.com>
2025-04-16 22:34:46 +0200

    styling pictures after load (app Fuel)

--- app/app.js
+++ app/app.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const mainDocElement = document.querySelector('.main');
-// let imgs;
-// let lesserBoundingHorizontal
+const appName = document.getElementsByTagName('body')[0].dataset.app;
+// let debugData = 'debug: <br>';
 
 let isMobile;
 if (navigator.userAgent.match(/(android|iphone|ipad)/i) != null || navigator.userAgentData.mobile == true) {
@@ -11,24 +11,40 @@ if (navigator.userAgent.match(/(android|iphone|ipad)/i) != null || navigator.use
     isMobile = false;
 }
 
+function styleImgs([img, passedWidth]) {
+    if (img.width >= passedWidth - 60 /* 20 margin mainDoc + 20 (marginL slike) + 20 (margR slike); */
+        && img.width < img.height /* samo za pokončne slike za zdaj */ ) {
+            img.style.marginLeft = '20px';
+            img.style.marginRight = '40px';
+            img.style.marginBottom = '30px';
+            img.width = passedWidth - 80;  // ker zdaj je 20 margin mainDoc + 20 margin L slike + 40 marginR slike; marginR mainDoc (30?) ne vpliva (vsaj ne, če imaš vodoravne slike ali ne)
+    }
+}
+
 if (isMobile) {
     mainDocElement.style.marginTop = '30px';
     mainDocElement.style.marginRight = '30px';
     mainDocElement.style.marginLeft = '20px';
 
-    // predvsem za Fuel, ker Igrajmo se ima majhne slike, povrh imajo velikost določeno na element level;
-    const lesserBoundingHorizontal = window.innerWidth > screen.width ? screen.width : window.innerWidth;
-    console.log(lesserBoundingHorizontal)
-    const imgs = document.getElementsByClassName('pics');
-    for (let i = 0; i < imgs.length; i++) {
-        if (imgs[i].width >= lesserBoundingHorizontal - 60 /* 20 margin mainDoc + 20 (marginL slike) + 20 (margR slike); */
-            && imgs[i].width < imgs[i].height /* samo za pokončne slike za zdaj */ ) {
-                imgs[i].style.marginLeft = '20px';
-                imgs[i].style.marginRight = '40px';
-                imgs[i].style.marginBottom = '30px';
-                imgs[i].width = lesserBoundingHorizontal - 80;  // ker zdaj je 20 margin mainDoc + 20 margin L slike + 40 marginR slike; marginR mainDoc (30?) ne vpliva (vsaj ne, če imaš vodoravne slike ali ne)
+    if (appName == 'fuel') {    // predvsem za Fuel, ker Igrajmo se ima majhne slike, povrh imajo velikost določeno na element level;
 
+        // const debugPurpose = document.getElementById('signature');
+
+        const lesserBoundingHorizontal = window.innerWidth > screen.width ? screen.width : window.innerWidth;
+        console.log(lesserBoundingHorizontal)
+        const imgs = document.getElementsByClassName('pics');
+        for (let i = 0; i < imgs.length; i++) {
+            if (imgs[i].complete) {
+                styleImgs([imgs[i], lesserBoundingHorizontal]);
+                // debugData += `complete, ${i}, false;<br>`;   // false pomeni, da ni bil klican listener;
+            } else {
+                imgs[i].addEventListener('load', styleImgs.bind(null, [imgs[i], lesserBoundingHorizontal]))
+                // debugData += `not complete, ${i}, true;<br>`;    // true pomeni, da je bil klican listener;
+            }
+            imgs[i].style.border = '#83a2b8 solid 1px'; // samo fuel dobijo obrobo, pri igrajmose ni videt lepo;
         }
+        // debugPurpose.insertAdjacentHTML('beforeBegin', debugData);
     }
+    
 
 }
