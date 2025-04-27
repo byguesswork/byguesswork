e0a0bd14365f18085c8d928e8b416c1ed3447a08
byguesswork <byguesswork@gmail.com>
2025-04-27 13:40:00 +0200

    3D models

---
+++ progs/3D_models/classes.js
@@ -0,0 +1,292 @@
+class Thingy {
+
+    static ctx;
+
+    constructor () {
+        // prazen;
+    }
+
+    draw(screenPoints, connections) {
+        Thingy.ctx.beginPath();
+        Thingy.ctx.moveTo(screenPoints[0].x, screenPoints[0].y);   // na začetku se vedno najprej pomakneš na izhodiščno točko (0-ta točka v arrayu scrnPts);
+        connections.forEach(element => {  // samo napisalo tako: connections.array.forEach
+            if (element.length == 1) {  // če je array element (ki je del arraya connections) dolg 1, podaja samo končno točko poteze (vzeto iz arraya scrnPts);
+                ctx.lineTo(screenPoints[element[0]].x, screenPoints[element[0]].y);
+            } else {                    // če je array element (ki je del arraya connections) dolg 2, podaja novo izhodišče in nato končno točko poteze;
+                ctx.moveTo(screenPoints[element[0]].x, screenPoints[element[0]].y);
+                ctx.lineTo(screenPoints[element[1]].x, screenPoints[element[1]].y);
+            }
+        });
+        Thingy.ctx.stroke();
+    }
+
+    move(spacePoint){
+        if (typeof spacePoint == 'string') {
+            if (spacePoint == LEFT) {
+                this.spacePoints.forEach(el => el.x -= 0.2);
+                this.draw(calcScreenPts(this.spacePoints));
+            } else if (spacePoint == RIGHT) {
+                this.spacePoints.forEach(el => el.x += 0.2);
+                this.draw(calcScreenPts(this.spacePoints));
+            } else if (spacePoint == CLOSER) {
+                this.spacePoints.forEach(el => el.y -= 0.5);
+                this.draw(calcScreenPts(this.spacePoints));
+            } else if (spacePoint == FAR) {
+                this.spacePoints.forEach(el => el.y += 0.5);
+                this.draw(calcScreenPts(this.spacePoints));
+            }
+        }
+    }
+
+
+    static meetCtx(passedCtx){
+        Thingy.ctx = passedCtx;
+    }
+}
+
+
+class SpacePoint{
+    constructor (xWidth, yDepth, zHeight) {
+        this.x = xWidth; // širina prostora; kar je na ekranu levo desno
+        this.y = yDepth; // globina prostora; kar bi bilo na ekranu naprej, v globino, daleč od gledalca;
+        this.z = zHeight; // višina prostora; kar je na ekranu gor/dol;
+    }
+}
+
+class ScreenPoint{
+    constructor (x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+
+class Square extends Thingy {
+
+    constructor(spacePoint, side) {
+        super();
+
+        this.spacePoints = [new SpacePoint(), new SpacePoint(), new SpacePoint(), new SpacePoint()];
+        this.side = side;
+        // connections so navodila, med katerimi točkami je treba risat povezave; začetek v točki 0 je privzet (v metodi draw());
+        this.connections = [[1], [2], [3], [0]];    // connections za kvadrat podaja samo točke za zvezno risanje, vmes nobenega novega izhodišča (novo izhodišče bi bilo, če bi kateri od elementov arraya connections bil array z dvema elementoma);
+
+        this.move(spacePoint);          
+    }
+    
+    // v bistvu uporabimo isto metodo za prvič postavit na zemjevid in vsakokrat naslednjič;
+    move(spacePoint){ // vedno (pri inicializaciji in morebitnem poznejšem move) podaš izhodiščno točko, ki je spodnja leva; morda v prihodnje naredit tudi varianto metode, da podaš samo zamik;
+        this.spacePoints[0].x = spacePoint.x; // 0 je spodnji levi;
+        this.spacePoints[0].y = spacePoint.y;
+        this.spacePoints[0].z = spacePoint.z;
+        // y-ji so vsi enaki;
+        this.spacePoints[1].y = spacePoint.y;
+        this.spacePoints[2].y = spacePoint.y;
+        this.spacePoints[3].y = spacePoint.y;
+        // še ostala oglišča, v kontra smeri urinega kazalca;
+        this.spacePoints[1].x = spacePoint.x + this.side;
+        this.spacePoints[1].z = spacePoint.z;
+        this.spacePoints[2].x = spacePoint.x + this.side;
+        this.spacePoints[2].z = spacePoint.z - this.side;
+        this.spacePoints[3].x = spacePoint.x;
+        this.spacePoints[3].z = spacePoint.z - this.side;
+
+    }
+
+    draw(scrnPts){
+        super.draw(scrnPts, this.connections);
+    }
+}
+
+
+class Cube extends Thingy {
+
+    constructor(passedSpacePoint, side){
+        super();
+
+        this.spacePoints = new Array(8);
+        for (let i = 0; i <= 7; i++) { this.spacePoints[i] = new SpacePoint(); }
+        this.side = side;
+        this.connections = [[1], [2], [3], [0], [4, 5], [6], [7], [4], [0, 4], [1, 5], [2, 6], [3, 7],];    // connections za kocko najprej nariše sprednji kvadrat (0,1,2,3), potem zadnjega (4,5,6,7) in potem povezovalne črte;
+
+        this.move(passedSpacePoint);    // v bistvu uporabimo isto metodo za prvič postavit na zemljevid in vsakokrat naslednjič;
+    }
+
+    move(spacePoint){
+        if (typeof spacePoint == "object") {
+            this.spacePoints[0].x = spacePoint.x; // 0 je spodnji levi kot prednjega kvadrata;
+            this.spacePoints[0].y = spacePoint.y;
+            this.spacePoints[0].z = - spacePoint.z; // z-je treba podajat negativno !!!! ker se prostorska z os preslika v y os na zaslonu, y pa narađća navzdol
+            // skratka, če podaš prostorske koordinate (-1, 5, -1), tj, malo levo dol od sredine ekrana, se v spacePoint[0] to shrani kot (-1, 5, 1) ..
+            // .. tj. z v spacepoint je notiran inverzno, da se potem lahko pravilno pretvori v koordinato y na ploskvi canvasa;
+    
+            // preostali y-i prednjega kvadrata so vsi enaki;
+            this.spacePoints[1].y = spacePoint.y;
+            this.spacePoints[2].y = spacePoint.y;
+            this.spacePoints[3].y = spacePoint.y;
+            // še ostala oglišča prednjega kvadrata, v kontra smeri urinega kazalca;
+            this.spacePoints[1].x = spacePoint.x + this.side;
+            this.spacePoints[1].z = - spacePoint.z;
+            this.spacePoints[2].x = spacePoint.x + this.side;
+            this.spacePoints[2].z = - spacePoint.z - this.side; // zato se tudi side odšteje, ne pa prišteje; LAHKO PA BI MORDA vse imel tu na plus in samo pri preračunu na ploskev dal na minus;
+            this.spacePoints[3].x = spacePoint.x;
+            this.spacePoints[3].z = - spacePoint.z - this.side;
+    
+            // zadnji kvadrat;
+            // y-i zadnjega kvadrata so vsi enaki;
+            this.spacePoints[4].y = spacePoint.y + this.side;
+            this.spacePoints[5].y = spacePoint.y + this.side;
+            this.spacePoints[6].y = spacePoint.y + this.side;
+            this.spacePoints[7].y = spacePoint.y + this.side;
+            // še ostala oglišča zadnjega kvadrata, v kontra smeri urinega kazalca;
+            this.spacePoints[4].x = spacePoint.x;
+            this.spacePoints[4].z = - spacePoint.z;
+            this.spacePoints[5].x = spacePoint.x + this.side;
+            this.spacePoints[5].z = - spacePoint.z;
+            this.spacePoints[6].x = spacePoint.x + this.side;
+            this.spacePoints[6].z = - spacePoint.z - this.side;
+            this.spacePoints[7].x = spacePoint.x;
+            this.spacePoints[7].z = - spacePoint.z - this.side;
+        } else if (typeof spacePoint == 'string') {
+            super.move(spacePoint);
+        } 
+    }
+
+    draw(scrnPts){
+        super.draw(scrnPts, this.connections);
+    }
+}
+
+
+class Pickup extends Thingy {
+    constructor(passedSpacePoint){  // gledan od spredaj, točka spodaj levo
+        super();
+
+        this.spacePoints = new Array(8);
+        for (let i = 0; i <= 7; i++) { this.spacePoints[i] = new SpacePoint(); }
+        this.connections = [[1], [2], [3], [0], [4, 5], [6], [7], [4], [0, 4], [1, 5], [2, 6], [3, 7], [3, 8], [2, 9], [8, 9], [10], [11], [8], [10, 12], [13], [11], [13, 15], [14], [12]];
+
+        this.move(passedSpacePoint);    // v bistvu uporabimo isto metodo za prvič postavit na zemljevid in vsakokrat naslednjič;
+    }
+
+    move(spacePoint){
+        if (typeof spacePoint == "object") {
+            this.spacePoints[0].x = spacePoint.x; // 0 je spodnji levi kot karoserije, gledano od sprefaj;
+            this.spacePoints[0].y = spacePoint.y;
+            this.spacePoints[0].z = - spacePoint.z; // z-je treba podajat negativno !!!! ker se prostorska z os preslika v y os na zaslonu, y pa narašća navzdol; za razlago glej Cube;
+    
+            // preostali y-i prednjega kvadrata so vsi enaki;
+            this.spacePoints[1].y = spacePoint.y;
+            this.spacePoints[2].y = spacePoint.y;
+            this.spacePoints[3].y = spacePoint.y;
+            // še ostala oglišča prednjega kvadrata, v kontra smeri urinega kazalca;
+            this.spacePoints[1].x = spacePoint.x + 1.85;    // širina kabine
+            this.spacePoints[1].z = - spacePoint.z;
+            this.spacePoints[2].x = spacePoint.x + 1.85;
+            this.spacePoints[2].z = - spacePoint.z - 0.9; // višina spodnjega dela karoserije; side se odšteje, ne pa prišteje (razlago glej Cube);
+            this.spacePoints[3].x = spacePoint.x;
+            this.spacePoints[3].z = - spacePoint.z - 0.9;
+    
+            // zadnji kvadrat karoserij;
+            // y-i zadnjega kvadrata so vsi enaki;
+            this.spacePoints[4].y = spacePoint.y + 4.7; // dolžina
+            this.spacePoints[5].y = spacePoint.y + 4.7;
+            this.spacePoints[6].y = spacePoint.y + 4.7;
+            this.spacePoints[7].y = spacePoint.y + 4.7;
+            // še ostala oglišča zadnjega kvadrata, v kontra smeri urinega kazalca;
+            this.spacePoints[4].x = spacePoint.x;
+            this.spacePoints[4].z = - spacePoint.z;
+            this.spacePoints[5].x = spacePoint.x + 1.85;
+            this.spacePoints[5].z = - spacePoint.z;
+            this.spacePoints[6].x = spacePoint.x + 1.85;
+            this.spacePoints[6].z = - spacePoint.z - 0.9;
+            this.spacePoints[7].x = spacePoint.x;
+            this.spacePoints[7].z = - spacePoint.z - 0.9;
+
+            this.spacePoints.push(new SpacePoint(spacePoint.x + 0.05, spacePoint.y + 1.0, - spacePoint.z - 1.1)); // 8   ; 0,1, da je malo ožje kot karoserija spodaj
+            this.spacePoints.push(new SpacePoint(spacePoint.x + 1.8, spacePoint.y + 1.0, - spacePoint.z - 1.1));    // 9 ; 1.75, da je malo ožje kot karoserija spodaj (1,85); točki pregiba preden se havba, ki se dviguje, začne dvigovat v steklo;
+            this.spacePoints.push(new SpacePoint(spacePoint.x + 1.75, spacePoint.y + 1.7, - spacePoint.z - 1.8)); // 10   ; desna zgornja točka stekla (gledano od spredaj); dolžina tega dela: 0,7, končan višina: 1,8
+            this.spacePoints.push(new SpacePoint(spacePoint.x + 0.1, spacePoint.y + 1.7, - spacePoint.z - 1.8));    // 11 ; leva zgornja stekla
+            this.spacePoints.push(new SpacePoint(spacePoint.x + 1.75, spacePoint.y + 2.4, - spacePoint.z - 1.8)); // 12   ; desna zadnja zgornja točka kabine (gledano od spredaj); ravna streha kabine dolga 0,7
+            this.spacePoints.push(new SpacePoint(spacePoint.x + 0.1, spacePoint.y + 2.4, - spacePoint.z - 1.8));    // 13 ; leva zadnja zgornja kabine
+            this.spacePoints.push(new SpacePoint(spacePoint.x + 1.85, spacePoint.y + 2.4, - spacePoint.z - 0.9)); // 14   ; desna zadnja spodnja točka kabine (na sredini karoserije, na pol poti od tli do vrha; gledano od spredaj)
+            this.spacePoints.push(new SpacePoint(spacePoint.x + 0.0, spacePoint.y + 2.4, - spacePoint.z - 0.9));    // 15 ; leva zadnja spodnja kabine (na sredini med tlemi in vrhom kabine)
+
+        } else if (typeof spacePoint == 'string') {
+            super.move(spacePoint);
+        } 
+    }
+
+    draw(scrnPts){
+        super.draw(scrnPts, this.connections);
+    }
+
+}
+
+class Connection extends Thingy {
+
+    constructor(spacePoint, spacePoint2) {
+        super();
+
+        this.spacePoints = [spacePoint, spacePoint2];
+        // connections so navodila, med katerimi točkami je treba risat povezave; začetek v točki 0 je privzet (v metodi draw());
+        this.connections = [[0, 1]];
+
+        // tale nima move();
+    }
+    
+    // v bistvu uporabimo isto metodo za prvič postavit na zemljevid in vsakokrat naslednjič;
+    move(spacePoint){
+        if (typeof spacePoint == "object") {
+            // prazno;
+        } else if (typeof spacePoint == 'string') {
+            super.move(spacePoint);
+        } 
+    }
+
+    draw(scrnPts){
+        super.draw(scrnPts, this.connections);
+    }
+}
+
+
+class HorzRectangle extends Thingy {
+
+    constructor(spacePoint, width, depth) {
+        super();
+
+        this.spacePoints = [new SpacePoint(), new SpacePoint(), new SpacePoint(), new SpacePoint()];
+        this.width = width;
+        this.depth = depth;
+        // connections so navodila, med katerimi točkami je treba risat povezave; začetek v točki 0 je privzet (v metodi draw());
+        this.connections = [[1], [2], [3], [0]];    // connections za kvadrat podaja samo točke za zvezno risanje, vmes nobenega novega izhodišča (novo izhodišče bi bilo, če bi kateri od elementov arraya connections bil array z dvema elementoma);
+
+        this.move(spacePoint);          
+    }
+    
+    // v bistvu uporabimo isto metodo za prvič postavit na zemjevid in vsakokrat naslednjič;
+    move(spacePoint){
+        if (typeof spacePoint == "object") {
+            this.spacePoints[0].x = spacePoint.x; // 0 je sprednji (bližnji) levi;
+            this.spacePoints[0].y = spacePoint.y;
+            this.spacePoints[0].z = spacePoint.z;
+            // z-ji so vsi enaki;
+            this.spacePoints[1].z = spacePoint.z;
+            this.spacePoints[2].z = spacePoint.z;
+            this.spacePoints[3].z = spacePoint.z;
+            // še ostala oglišča, v kontra smeri urinega kazalca;
+            this.spacePoints[1].x = spacePoint.x + this.width;
+            this.spacePoints[1].y = spacePoint.y;
+            this.spacePoints[2].x = spacePoint.x + this.width;
+            this.spacePoints[2].y = spacePoint.y + this.depth;
+            this.spacePoints[3].x = spacePoint.x;
+            this.spacePoints[3].y = spacePoint.y + this.depth;
+        } else if (typeof spacePoint == 'string') {
+            super.move(spacePoint);
+        } 
+    }
+
+    draw(scrnPts){
+        super.draw(scrnPts, this.connections);
+    }
+}
---
+++ progs/3D_models/threeDmodels.js
@@ -0,0 +1,163 @@
+'use strict';
+
+const wdth = window.innerWidth - 5; // -5 , da ni skrolbara;
+const hght = window.innerHeight - 5;
+const canvas = document.getElementById('canvas');
+canvas.height = hght;
+canvas.width = wdth;
+const ctx = canvas.getContext('2d');
+const bckgndColr = '#4e4e4c';
+const lineColor = '#f0fff0';
+ctx.lineWidth = 1;
+ctx.strokeStyle = lineColor;
+Thingy.meetCtx(ctx);
+
+const midPoint = {
+    x: wdth % 2 == 0 ? wdth / 2 : wdth / 2 + 0.5, 
+    y: hght % 2 == 0 ? hght / 2 : hght / 2 + 0.5
+}
+// koti; ekran od leve do desne je 3.0 radiana (malo manj kot 180', kao oponaša vidno polje človeka), pomeni da je midPoint.x = 1.5 (ali tam nekje); zdaj izračunamo še, kolikšen kot je lahko po vertikali 
+const FISHEYE = 1.45;
+const TELEANGLE = 0.25;
+let hrzRadsFrmCentr = FISHEYE;
+let vertRadsFrmCentr;
+calcVertRadsFrmCentr();
+function calcVertRadsFrmCentr(){
+    vertRadsFrmCentr = (Math.round(10000 * (midPoint.y * hrzRadsFrmCentr) / midPoint.x)) / 10000;
+}  
+console.log(wdth, hght, midPoint, vertRadsFrmCentr);
+
+function calcScreenPts(spacePoints) {
+    const scrnPts = new Array();
+    spacePoints.forEach(element => {
+        const scrnPt = new ScreenPoint();
+        //x
+        // glavna logika je Math.atan2(offset(od navpičnice, vodoravnice), razdalja do tja);
+        scrnPt.x = midPoint.x + (Math.atan2(element.x - viewer.x, ((element.y + viewer.y)**2 + (element.z + viewer.z)**2 )**(1/2))/hrzRadsFrmCentr) * midPoint.x;
+            // (element.y**2 + element.z**2)**(1/2) - je glavna scena, da upošteva, da se recimo nebotičnik proti navzgor oža;
+
+        // y;
+        scrnPt.y = midPoint.y + (Math.atan2(element.z + viewer.z, ((element.y + viewer.y)**2  + (element.x - viewer.x)**2 )**(1/2))/vertRadsFrmCentr) * midPoint.y;
+
+        scrnPts.push(scrnPt);
+    });
+
+    return scrnPts;
+
+}
+
+function clearCanvas() {
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = bckgndColr;
+    ctx.fillStyle = bckgndColr;
+    ctx.beginPath();
+    ctx.rect(1, 1, wdth, hght);
+    ctx.fill();
+    ctx.stroke();
+    ctx.strokeStyle = lineColor;
+}
+
+const viewer = new SpacePoint(0, 0, 1.7);
+// na začetku ima gledalec privzeto spacePoint 0,0,1.7 (kao visok 1.7m) in gleda naravnost naprej vzdolž osi y, torej v {0,neskončno,1.7}
+
+// - - - - - -  DODAJANJE STVARI - - - - - -
+const cubes = [];
+// navpične kocke;
+for (let i = 0; i <= 16; i += 4) {
+    cubes.push(new Cube(new SpacePoint(6, 20, i), 4))
+};
+// vodoravne kocke;
+for (let i = 10; i <= 46; i += 4) {
+    cubes.push(new Cube(new SpacePoint(i, 20, 0), 4))
+};
+
+const pickupTruck = new Pickup(new SpacePoint(5, 5, 0));
+
+const line1 = new Connection(new SpacePoint(-4, 0, 0), new SpacePoint(-4, 2000, 0));
+const line2 = new Connection(new SpacePoint(4, 0, 0), new SpacePoint(4, 2000, 0));
+const dividingLines = [];
+for (let i = 2; i <= 302; i += 10) {
+    dividingLines.push(new HorzRectangle(new SpacePoint(-0.1, i, 0), 0.2, 3))
+};
+
+
+//  - - - - - -   IZRIS DODANIH STVARI   - - - - - -
+const activeItems = [line1, line2, ...dividingLines, ...cubes, pickupTruck];
+activeItems.forEach(elt => {
+    elt.draw(calcScreenPts(elt.spacePoints))
+});
+
+
+// - - - -  CONTROLS  - - - - - -
+document.addEventListener('keydown', atKeyPress);
+const LEFT = 'l';
+const RIGHT = "r";
+const CLOSER = "c";
+const FAR = 'f';
+
+function atKeyPress(e){
+    if (e.key == 'ArrowLeft') {
+        clearCanvas();
+        activeItems.forEach(el =>  el.move(LEFT));
+    } else if (e.key == 'ArrowRight') {
+        clearCanvas();
+        activeItems.forEach(el => el.move(RIGHT));
+    } else if (e.code == 'KeyC') {
+        clearCanvas();
+        activeItems.forEach(el => el.move(CLOSER));
+    } else if (e.code == 'KeyF') {
+        clearCanvas();
+        activeItems.forEach(el => el.move(FAR));
+    }
+}
+
+const lensBtns = document.getElementsByClassName('lens');
+lensBtns[0].addEventListener('click', lensBtnOprtn);
+lensBtns[1].addEventListener('click', lensBtnOprtn);
+function lensBtnOprtn(evt){
+    if (evt.target.classList.contains('lens-unselected')) {
+        if (lensBtns[0].classList.contains('lens-selected')) {
+            changeLens(true);
+        } else changeLens(false);
+        lensBtns[0].classList.toggle('lens-selected');
+        lensBtns[1].classList.toggle('lens-selected');
+        lensBtns[0].classList.toggle('lens-unselected');
+        lensBtns[1].classList.toggle('lens-unselected');
+    }
+
+    function changeLens(doFish){
+        if (doFish) hrzRadsFrmCentr = FISHEYE;
+        else hrzRadsFrmCentr = TELEANGLE;
+        calcVertRadsFrmCentr();
+        clearCanvas();
+        activeItems.forEach(element => {
+            element.draw(calcScreenPts(element.spacePoints))
+        });        
+    }
+}
+
+const zoomBtns = document.getElementsByClassName('zoom');
+zoomBtns[0].addEventListener('click', zoomBtnsOprtn);
+zoomBtns[1].addEventListener('click', zoomBtnsOprtn);
+function zoomBtnsOprtn(e){
+    if (e.target == zoomBtns[0]) {
+        clearCanvas();
+        activeItems.forEach(el => el.move(CLOSER));
+    }   else {
+        clearCanvas();
+        activeItems.forEach(el => el.move(FAR));
+    }
+}
+
+const panBtns = document.getElementsByClassName('pan');
+panBtns[0].addEventListener('click', panBtnsOprtn);
+panBtns[1].addEventListener('click', panBtnsOprtn);
+function panBtnsOprtn(e){
+    if (e.target == panBtns[0]) {
+        clearCanvas();
+        activeItems.forEach(el => el.move(LEFT));
+    }   else {
+        clearCanvas();
+        activeItems.forEach(el => el.move(RIGHT));
+    }
+}
