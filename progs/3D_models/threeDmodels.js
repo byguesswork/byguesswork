0777cce4319c848427d384300315dfca50d2765c
byguesswork <byguesswork@gmail.com>
2025-04-27 17:58:03 +0200

    3d upd

--- progs/3D_models/threeDmodels.js
+++ progs/3D_models/threeDmodels.js
@@ -73,8 +73,18 @@ for (let i = 10; i <= 46; i += 4) {
 
 const pickupTruck = new Pickup(new SpacePoint(5, 5, 0));
 
-const line1 = new Connection(new SpacePoint(-4, 0, 0), new SpacePoint(-4, 2000, 0));
-const line2 = new Connection(new SpacePoint(4, 0, 0), new SpacePoint(4, 2000, 0));
+// const line1 = new Connection(new SpacePoint(-4, 0, 0), new SpacePoint(-4, 2000, 0));
+const line1_1 = new Connection(new SpacePoint(-4, 0, 0), new SpacePoint(-4, 2, 0));
+const line1_2 = new Connection(new SpacePoint(-4, 2, 0), new SpacePoint(-4, 4, 0));
+const line1_3 = new Connection(new SpacePoint(-4, 4, 0), new SpacePoint(-4, 8, 0));
+const line1_4 = new Connection(new SpacePoint(-4, 8, 0), new SpacePoint(-4, 16, 0));
+const line1_5 = new Connection(new SpacePoint(-4, 16, 0), new SpacePoint(-4, 2000, 0));
+const line2 = new Connection(new SpacePoint(4, 0, 0), new SpacePoint(4, 2000, 0));  // opazi, kako je line2 ravna in tudi posga v avto, ko grejo predmeti zadosti desno; razdeljena linija 2 pa je OK z avtom;
+const line2_1 = new Connection(new SpacePoint(4, 0, 0), new SpacePoint(4, 2, 0));
+const line2_2 = new Connection(new SpacePoint(4, 2, 0), new SpacePoint(4, 4, 0));
+const line2_3 = new Connection(new SpacePoint(4, 4, 0), new SpacePoint(4, 8, 0));
+const line2_4 = new Connection(new SpacePoint(4, 8, 0), new SpacePoint(4, 16, 0));
+const line2_5 = new Connection(new SpacePoint(4, 16, 0), new SpacePoint(4, 2000, 0));
 const dividingLines = [];
 for (let i = 2; i <= 302; i += 10) {
     dividingLines.push(new HorzRectangle(new SpacePoint(-0.1, i, 0), 0.2, 3))
@@ -82,7 +92,8 @@ for (let i = 2; i <= 302; i += 10) {
 
 
 //  - - - - - -   IZRIS DODANIH STVARI   - - - - - -
-const activeItems = [line1, line2, ...dividingLines, ...cubes, pickupTruck];
+const activeItems = [/*line1,*/ line1_1, line1_2, line1_3, line1_4, line1_5, line2, line2_1, line2_2,
+    line2_3, line2_4, line2_5, ...dividingLines, ...cubes, pickupTruck];
 activeItems.forEach(elt => {
     elt.draw(calcScreenPts(elt.spacePoints))
 });
@@ -95,20 +106,16 @@ const RIGHT = "r";
 const CLOSER = "c";
 const FAR = 'f';
 
+function moveItems(toWhere){
+    clearCanvas();
+    activeItems.forEach(el => el.move(toWhere));
+}
+
 function atKeyPress(e){
-    if (e.key == 'ArrowLeft') {
-        clearCanvas();
-        activeItems.forEach(el =>  el.move(LEFT));
-    } else if (e.key == 'ArrowRight') {
-        clearCanvas();
-        activeItems.forEach(el => el.move(RIGHT));
-    } else if (e.code == 'KeyC') {
-        clearCanvas();
-        activeItems.forEach(el => el.move(CLOSER));
-    } else if (e.code == 'KeyF') {
-        clearCanvas();
-        activeItems.forEach(el => el.move(FAR));
-    }
+    if (e.key == 'ArrowLeft') { moveItems(LEFT) }
+    else if (e.key == 'ArrowRight') { moveItems(RIGHT);}
+    else if (e.code == 'KeyC') { moveItems(CLOSER) }
+    else if (e.code == 'KeyF') { moveItems(FAR)  }
 }
 
 const lensBtns = document.getElementsByClassName('lens');
@@ -140,24 +147,14 @@ const zoomBtns = document.getElementsByClassName('zoom');
 zoomBtns[0].addEventListener('click', zoomBtnsOprtn);
 zoomBtns[1].addEventListener('click', zoomBtnsOprtn);
 function zoomBtnsOprtn(e){
-    if (e.target == zoomBtns[0]) {
-        clearCanvas();
-        activeItems.forEach(el => el.move(CLOSER));
-    }   else {
-        clearCanvas();
-        activeItems.forEach(el => el.move(FAR));
-    }
+    if (e.target == zoomBtns[0]) { moveItems(CLOSER); }
+    else { moveItems(FAR); }
 }
 
 const panBtns = document.getElementsByClassName('pan');
 panBtns[0].addEventListener('click', panBtnsOprtn);
 panBtns[1].addEventListener('click', panBtnsOprtn);
 function panBtnsOprtn(e){
-    if (e.target == panBtns[0]) {
-        clearCanvas();
-        activeItems.forEach(el => el.move(LEFT));
-    }   else {
-        clearCanvas();
-        activeItems.forEach(el => el.move(RIGHT));
-    }
+    if (e.target == panBtns[0]) { moveItems(LEFT); }
+    else { moveItems(RIGHT); }
 }
